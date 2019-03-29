5978b2e8500cec17d47aa50f0ee7e1301f85d5c6
Tyler Templeton <blackmage9999@gmail.com>
2019-03-29 13:14:23 -0400

    Added Hello World Application

---
+++ 01_HelloWorld/01_HelloWorld/_01_HelloWorld.py
@@ -0,0 +1,16 @@
+
+#The purpose of this program is to create a Hello World application that Prints Hello World and askes the users name and prints it
+
+#Create multiline string variable
+HelloWorld = """
+-------------------------
+Hello World
+-------------------------
+"""
+
+#Print string
+print(HelloWorld)
+
+#User Input
+name = input ("What's your name?")
+print ("Hello,  "+ name)
